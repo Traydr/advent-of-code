d114495fe91e9af22a204bb6a2138f135a38ae0a
Titas Lukaitis <Traydr@protonmail.com>
2021-12-04 18:50:47 +0100

    Puzzle 3 Part 2 WIP

--- All_Puzzles/Scripts/Puz3.cs
+++ All_Puzzles/Scripts/Puz3.cs
@@ -8,7 +8,10 @@ namespace Puzzle_1.Scripts
 
         public static void RunPuzzle()
         {
+            Console.WriteLine("\n Puzzle 3 Part 1");
             PartOne();
+            Console.WriteLine("\n Puzzle 3 Part 2");
+            PartTwo();
         }
 
         public static void PartOne()
@@ -65,18 +68,93 @@ namespace Puzzle_1.Scripts
                 }
             }
 
-            for (int i = binNumLen - 1; i > 0; i--)
-            {
-                
-            }
-            
+            gammaRate = Convert.ToInt32(gammaRateBin, 2);
+            epsilonRate = Convert.ToInt32(epsilonRateBin, 2);
+
             Console.WriteLine("BIN 1:{0}, \n BIN2:{1}", gammaRateBin, epsilonRateBin);
+            Console.WriteLine("Final Answer: {0}", gammaRate * epsilonRate);
             // Answer: 2583164
         }
         
         public static void PartTwo()
         {
-            
+            string[] allLines = _localUtils.ReadAllLinesFromPuzzleInput(3);
+            int binNumLen = allLines[0].Length;
+            int[,] binBitAvg = new int[binNumLen, 2];
+            string oxGenRatingBin = "";
+            string co2ScrubRatingBin = "";
+            int oxGenRating = 0;
+            int co2ScrubRating = 0;
+
+            for (int i = 0; i < binNumLen; i++)
+            {
+                if (allLines[i].Length < oxGenRatingBin.Length && allLines[i].Length < oxGenRatingBin.Length)
+                {
+                    continue;
+                }
+                
+                if (MostCommonBinInIndexArray(allLines, i, oxGenRatingBin, true))
+                {
+                    oxGenRatingBin += "1";
+                }
+                else
+                {
+                    oxGenRatingBin += "0";
+                }
+                
+                if (MostCommonBinInIndexArray(allLines, i, co2ScrubRatingBin, false))
+                {
+                    co2ScrubRatingBin += "0";
+                }
+                else
+                {
+                    co2ScrubRatingBin += "1";
+                }
+            }
+
+            oxGenRating = Convert.ToInt32(oxGenRatingBin, 2);
+            co2ScrubRating = Convert.ToInt32(co2ScrubRatingBin, 2);
+
+            Console.WriteLine(" -> BIN 1:{0}, \n -> BIN2:{1}", oxGenRatingBin, co2ScrubRatingBin);
+            Console.WriteLine(" -> Final Answer: {0}", oxGenRating * co2ScrubRating);
+            // Answer less than 3153975
+        }
+
+        private static bool MostCommonBinInIndexArray(string[] listOfBinNums, int index, string binSoFar, bool isOx)
+        {
+            int numOnes = 0;
+            int numZeros = 0;
+
+            for (int i = 0; i < listOfBinNums.Length; i++)
+            {
+                if (listOfBinNums[i][index] == '0' && IsInputPartOfString(binSoFar, listOfBinNums[i]))
+                {
+                    numZeros += 1;
+                }
+                else if (listOfBinNums[i][index] == '1' && IsInputPartOfString(binSoFar, listOfBinNums[i]))
+                {
+                    numOnes += 1;
+                }
+            }
+
+            if (isOx)
+            {
+                return numOnes >= numZeros;
+            }
+            else
+            {
+                return numOnes > numZeros;
+            }
+        }
+
+        private static bool IsInputPartOfString(string input, string compareTo)
+        {
+            string comparisionString = compareTo.Substring(0, input.Length);
+            if (comparisionString == input)
+            {
+                Console.WriteLine("Input {0} | Compared to {1} | Output {2}", input, compareTo, comparisionString == input);
+            }
+            return comparisionString == input;
         }
     }
 }
